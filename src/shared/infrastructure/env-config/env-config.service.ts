2a9284fd448838404e569cc31bd9decd2935ff6d
Mauricio Nunes <mauricom7@hotmail.com>
2025-07-17 13:38:42 -0300

    feat: implement EnvConfigService for environment variable management

--- src/shared/infrastructure/env-config/env-config.service.ts
+++ src/shared/infrastructure/env-config/env-config.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { EnvConfig } from './env-config.interface';
 
 @Injectable()
-export class EnvConfigService {}
+export class EnvConfigService implements EnvConfig {
+  constructor(private readonly configService: ConfigService) { }
+
+  getAppPort(): number {
+    return Number(this.configService.get<number>('PORT'))
+  }
+  getNodeEnv(): string {
+    const nodeEnv = this.configService.get<string>('NODE_ENV');
+    if (!nodeEnv) {
+      throw new Error('NODE_ENV is not set');
+    }
+    return nodeEnv;
+  }
+}
