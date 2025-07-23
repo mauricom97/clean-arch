4e8bd2f604e419dcdd87e707b05ec1aef798b033
Mauricio Nunes <mauricom7@hotmail.com>
2025-07-23 12:49:12 -0300

    feat: create constructor class User

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -1,1 +1,12 @@
-export class User {}
+export type UserProps = {
+  name: string,
+  email: string,
+  password: string,
+  createdAt?: Date
+}
+
+export class User {
+  constructor(public readonly props: UserProps) {
+    this.props.createdAt = this.props.createdAt ?? new Date()
+  }
+}
