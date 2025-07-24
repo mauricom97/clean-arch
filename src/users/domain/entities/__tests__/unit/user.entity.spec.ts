93195c444498f3a5b7501c5905d84fb0d5661d49
Mauricio Nunes <mauricom7@hotmail.com>
2025-07-24 12:44:53 -0300

    cria builder para geração de dados dos testes de usuarios

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -1,14 +1,10 @@
-import { faker } from '@faker-js/faker'
+import { UserDataBuilder } from '../../../testing/helpers/user-data-builder'
 import { UserEntity, UserProps } from '../../user.entity'
 describe('UserEntity unit tests', () => {
   let props: UserProps
   let sut: UserEntity
   beforeEach(() => {
-    props = {
-      name: faker.person.fullName(),
-      email: faker.internet.email(),
-      password: faker.internet.password()
-    }
+    props = UserDataBuilder({})
     sut = new UserEntity(props)
   })
 
