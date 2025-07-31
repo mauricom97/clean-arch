e02b98022b19c51796e2fe32223b2652259b96da
Mauricio Nunes <mauricom7@hotmail.com>
2025-07-31 20:04:17 -0300

    refatora a formatação do código na entidade de usuário

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -1,31 +1,34 @@
-import { Entity } from "../../../shared/domain/entities/entity"
+import { Entity } from '../../../shared/domain/entities/entity';
 
 export type UserProps = {
-  name: string,
-  email: string,
-  password: string,
-  createdAt?: Date
-}
+  name: string;
+  email: string;
+  password: string;
+  createdAt?: Date;
+};
 
 export class UserEntity extends Entity<UserProps> {
-  constructor(public readonly props: UserProps, id?: string) {
-    super(props, id)
-    this.props.createdAt = this.props.createdAt ?? new Date()
+  constructor(
+    public readonly props: UserProps,
+    id?: string,
+  ) {
+    super(props, id);
+    this.props.createdAt = this.props.createdAt ?? new Date();
   }
 
   get name() {
-    return this.props.name
+    return this.props.name;
   }
 
   get email() {
-    return this.props.email
+    return this.props.email;
   }
 
   get password() {
-    return this.props.password
+    return this.props.password;
   }
 
   get createdAt() {
-    return this.props.createdAt
+    return this.props.createdAt;
   }
 }
